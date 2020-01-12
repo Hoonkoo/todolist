28365a4bde40f58b28f5600ff36a2c03a84f0f87
koooobah <koooobah@gmail.com>
2020-01-13 08:01:32 +0900

    user controller setting

---
+++ backend/src/api/v1/user/user.ctrl.ts
@@ -0,0 +1,7 @@
+import { Request, Response, NextFunction } from "express";
+
+// 유저 정보 조회
+export const loadUser = (req: Request, res: Response, next: NextFunction) => {};
+
+// 회원가입
+export const signUp = (req: Request, res: Response, next: NextFunction) => {};
