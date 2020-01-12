709a253ec4ce0a1b8cdcf990227a67098e26aaa5
hoonha <sosqsz2@naver.com>
2020-01-12 09:36:52 +0900

    react router setting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import TodoTemplate from './TodoTemplate';
+import TodoLogin from './TodoLogin';
+import TodoRegister from './TodoRegister';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+import TodoListItem from './TodoListItem';
+import TodoDashBoard from './TodoDashBoard';
+import TodoDashBoardItem from "./TodoDashBoardItem";
+import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import TodoLoing from './TodoLogin';
+import Header from './component/Header';
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={TodoTemplate}/>
+        <Route path="/login" component={TodoLogin}/>
+      </Switch>
       
-    </div>
+    </>
   );
 }
 
