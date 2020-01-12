2f5055c5862682f9f51e4abd6ddd2e2606101d97
hoonha <sosqsz2@naver.com>
2020-01-12 20:12:03 +0900

    basic setting

--- frontend/src/TodoLogin.js
+++ frontend/src/TodoLogin.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 const TodoLogin = () => {
     return (
         <div>
-            12312321
+            <label htmlFor="email">Email</label>
+            <input type="email" /> 
+            <label htmlFor="password">password</label>
+            <input type="password"/>
+            <button>Sign in</button>
+
+            <Link to='/signup'>
+                <button>Sign up</button>
+            </Link>
         </div>
     );
 };
--- frontend/src/TodoTemplate.js
+++ frontend/src/TodoTemplate.js
@@ -1,24 +1,62 @@
-import React from 'react';
+import React , { useState } from 'react';
 import './TodoTemplate.scss';
-import TodoLogin from './TodoLogin';
 
-import { Route,Link } from 'react-router-dom';
+import TodoMain from './TodoMain';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+const date = new Date();
+const date_year = date.getFullYear();
+const date_month = date.getMonth()+1;
+const date_day = date.getDate();
+const new_date = (date_year +":"+ date_month +":"+ date_day);
+
 
 const TodoTemplate = ( ) => {
-    return (
+    const [isLogged , setIsLogged ] = useState(true);
+
+    const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "1",
+      isChecked: false,
+      date: new_date, 
+      fk_user_id:1
+    },
+    {
+      id: 2,
+      text: "2",
+      isChecked: true,
+      date: new_date, 
+      fk_user_id:1
+    },
+    {
+      id: 3,
+      text: "3",
+      isChecked: false,
+      date: new_date, 
+      fk_user_id:1
+    }
+    ]);
+
+    if (isLogged) {
+        return (
+            <TodoMain>
+                <TodoInput/>
+                <TodoList todos={todos}/>
+            </TodoMain>
+        );    
+    } else {
+        return(
         <div className="wrapper">
             <div className="content-title">
-                오늘 할 일을 
-                작성 해 주세요 
+                 오늘 할 일을 
+                 작성 해 주세요 
             </div>
-            <div className="content">
-               
-
-            </div>
-                
-            
         </div>
-    );
+        )
+    };
+
 };
 
 export default TodoTemplate;
