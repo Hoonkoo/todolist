ecc13649f713f58b4cf0d8a9b06097be9eb909f5
hoonha <sosqsz2@naver.com>
2020-01-28 23:35:06 +0900

    Today todos filter_v1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TodoTemplate from './TodoTemplate';
-import TodoLogin from './TodoLogin';
-
-
 import { Link, Route, Switch, BrowserRouter as Router } from "react-router-dom";
 
 import Header from './component/Header';
+import TodoTemplate from './TodoTemplate';
+import TodoLogin from './TodoLogin';
 import TodoSignup from './TodoSignup';
+import TodoDashBoard from './TodoDashBoard';
+
 function App() {
   return (
     <>
@@ -17,6 +17,7 @@ function App() {
         <Route exact path="/" component={TodoTemplate}/>
         <Route path="/login" component={TodoLogin}/>
         <Route path="/signup" component={TodoSignup}/>
+        <Route path="/DashBoard" component={TodoDashBoard}/> 
       </Switch>
       
     </>
